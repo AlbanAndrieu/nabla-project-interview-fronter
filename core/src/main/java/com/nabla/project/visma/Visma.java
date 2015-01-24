e4b1290afc21c778c9429e302f76623d2f870817
jenkins <root@localhost>
2015-01-24 14:52:56 -0600

    Cleaning

--- core/src/main/java/com/nabla/project/visma/Visma.java
+++ core/src/main/java/com/nabla/project/visma/Visma.java
@@ -36,7 +36,6 @@ package com.nabla.project.visma;
 import java.math.BigDecimal;
 import java.util.Scanner;
 
-import com.nabla.project.visma.api.ILoan;
 import com.nabla.project.visma.api.IProduct;
 
 @SuppressWarnings("all")
@@ -63,19 +62,7 @@ public class Visma
         }
 
         final IProduct product = new House(new BigDecimal(loanAmount));
-        final ILoan loan = new HouseLoan(product, numberOfYears, annualInterestRate);
 
-        System.out.println("Payment schedule is :\n " + loan.calcMonthlyPayment() + "\n");
-
-        final BigDecimal totalPayment = loan.getTotalPayment();
-        if (totalPayment.doubleValue() < loanAmount)
-        {
-            System.err.println("Total payment is lower than loan amount\n");
-            // Calculation is wrong
-        } else
-        {
-            System.out.println("Total payment is :\n " + totalPayment + "\n");
-        }
 
     }
 
