3efa9c5b863d2c26a39087bb239ce77ef66d13a9
jenkins <root@localhost>
2015-01-26 09:13:06 -0600

    Deactivate gatling perf test

--- gui/src/test/groovy/SampleITest.groovy
+++ gui/src/test/groovy/SampleITest.groovy
@@ -1,22 +1,22 @@
 import org.junit.Test
 
-import com.nabla.project.fronter.KataService
+import com.nabla.project.fronter.KataBowling
 import com.nabla.project.fronter.api.IGame
 import static org.junit.Assert.assertEquals
 
 class SampleITest extends GroovyTestCase {
 
     @Test
-    void testVisma() {
-        final IGame game = new KataService("XXX");
+    void testApplicatino() {
+        final IGame game = new KataBowling("XXX");
 
         System.out.println("Score is :\n " + game.getScore() + "\n");
 
-        assertEquals 30, game.getScore().size()
+        assertEquals 60, game.getScore()
         
         final Integer score = game.getScore();
 
-        assert "30" == score.toString();
+        assert "60" == score.toString();
     }
 
 }
